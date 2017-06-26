46c9e2051d59d931c34adbf9def731f1f30ca04c
eriol1977 <eriol1977@hotmail.com>
2017-06-26 21:20:51 +0200

    some validation

--- src/pages/expenses/expense.ts
+++ src/pages/expenses/expense.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
-import {NavParams, ViewController} from 'ionic-angular';
+import {NavParams, ViewController, ToastController} from 'ionic-angular';
 
-import { Type } from '../types/type';
-import { EXPENSE_TYPES } from '../types/types-provider';
+import {Type} from '../types/type';
+import {EXPENSE_TYPES} from '../types/types-provider';
 
 @Component({
     templateUrl: 'expense.html'
@@ -13,26 +13,53 @@ export class Expense {
     value: string;
     types: Type[] = EXPENSE_TYPES;
     notes: string;
-    
+
     // the following because [compareWith] doesn't work in the select component
     typeCode: string;
 
     constructor(public params: NavParams,
-        public viewCtrl: ViewController) {
+        public viewCtrl: ViewController,
+        public toastCtrl: ToastController) {
         this.date = this.params.get('date');
         this.value = this.params.get('value');
         this.notes = this.params.get('notes');
-        
+
         // the following because [compareWith] doesn't work in the select component
         this.typeCode = this.params.get('typeCode');
     }
 
     dismiss(save: boolean) {
+        if(save && !this.validateExpense())
+            return;
+
         // the following because [compareWith] doesn't work in the select component
         var type: any;
         this.types.forEach(t => {if (t.code == this.typeCode) type = t;});
-        
+
         let data = {'save': save, 'date': this.date, 'value': this.value, 'type': type, 'notes': this.notes};
         this.viewCtrl.dismiss(data);
+    }
+
+    private validateExpense() : boolean {
+        var result = true;
+        if (!this.date) {
+            this.alertMessage('Inserire una data');
+            result = false;
+        }else if (!this.value) {
+            this.alertMessage('Inserire un valore');
+            result = false;
+        }else if (!this.typeCode) {
+            this.alertMessage('Inserire un tipo di spesa');
+            result = false;
+        }
+        return result;
+    }
+
+    private alertMessage(msg: string) {
+        let toast = this.toastCtrl.create({
+            message: msg,
+            duration: 3000
+        });
+        toast.present();
     }
 }
